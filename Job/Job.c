9243567c4a58b45c32c9e0bb2e39bff31a23f51e
Bruno Di Stefano <bsdistefano@gmail.com>
2015-05-03 02:40:50 -0300

    Agrego archivo de configuracion para el Job

--- Job/Job.c
+++ Job/Job.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 #include <commons/log.h>
+#include <commons/config.h>
 
 //Declaración de funciones
 void conectaraMarta();
+void desconectarDeMarta();
+void configurar();
+
+//Declaración de variables
+t_config* configurador;
 
 int main(void){
+	configurar();
 	conectaraMarta(); //por el momento, solamente se implementa el Log
+	desconectarDeMarta(); //solo log
 	return 0;
+}
+
+void configurar(){
+	configurador= config_create("resources/jobConfig.conf");
+	printf("COMBINER=%s\n",config_get_string_value(configurador,"COMBINER"));
 }
 
 void conectaraMarta(){
 	t_log* logger;
 	logger = log_create("./jobLog.log", "Job", true, LOG_LEVEL_INFO); //se crea la instancia de log, que tambien imprimira en pantalla
 	log_info(logger,"Se conectó a MaRTA. IP: a.x.y.z, Puerto: xxxx\n"); //se agrega al log en modo de informacion la coneccion con MaRTA
+	log_destroy(logger); //se elimina la instancia de log
+	return;
+}
+
+void desconectarDeMarta(){
+	t_log* logger;
+	logger = log_create("./jobLog.log", "Job", true, LOG_LEVEL_INFO); //se crea la instancia de log, que tambien imprimira en pantalla
+	log_info(logger,"Se desconectó de MaRTA. IP: a.x.y.z, Puerto: xxxx\n"); //se agrega al log en modo de informacion la coneccion con MaRTA
 	log_destroy(logger); //se elimina la instancia de log
 	return;
 }
