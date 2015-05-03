4525435e7ed709fc4db56149f8c71a23d0f4bf7f
Bruno Di Stefano <bsdistefano@gmail.com>
2015-05-03 03:33:07 -0300

    Prueba de coneccion real entre Job y MaRTA

--- Job/Job.c
+++ Job/Job.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <commons/log.h>
 #include <commons/config.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 
 //Declaración de funciones
 void conectaraMarta();
@@ -18,14 +21,31 @@ int main(void){
 }
 
 void configurar(){
-	configurador= config_create("resources/jobConfig.conf");
-	printf("COMBINER=%s\n",config_get_string_value(configurador,"COMBINER"));
+	configurador= config_create("resources/jobConfig.conf"); //se asigna el archivo de configuración especificado en la ruta
+	printf("COMBINER=%s\n",config_get_string_value(configurador,"COMBINER")); //para probar que realmente tomó al archivo de configuración
 }
 
 void conectaraMarta(){
+	int sockfd, numbytes, new_fd;
+	struct sockaddr_in their_addr;
+
+	if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1){ //si función socket devuelve -1 es error
+        perror("socket");
+        exit(1);
+	}
+	their_addr.sin_family=AF_INET;
+	their_addr.sin_port=htons(config_get_int_value(configurador,"PUERTO_MARTA"));
+	their_addr.sin_addr.s_addr=inet_addr(config_get_string_value(configurador,"IP_MARTA"));
+	memset(&(their_addr.sin_zero),'\0',8);
+
+	if((new_fd=connect(sockfd,(struct sockaddr *)&their_addr,sizeof(struct sockaddr)))==-1){
+		perror("connect");
+		exit(1);
+	}
+
 	t_log* logger;
 	logger = log_create("./jobLog.log", "Job", true, LOG_LEVEL_INFO); //se crea la instancia de log, que tambien imprimira en pantalla
-	log_info(logger,"Se conectó a MaRTA. IP: a.x.y.z, Puerto: xxxx\n"); //se agrega al log en modo de informacion la coneccion con MaRTA
+	log_info(logger,"Se conectó a MaRTA. IP: %s, Puerto: %d\n",config_get_string_value(configurador,"IP_MARTA"),config_get_int_value(configurador,"PUERTO_MARTA")); //se agrega al log en modo de informacion la coneccion con MaRTA
 	log_destroy(logger); //se elimina la instancia de log
 	return;
 }
--- Marta/Marta.c
+++ Marta/Marta.c
@@ -1,4 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-//#include <breaking-C-library/>
+//Defino puerto donde va a estar escuchando procesos Job
+#define PUERTO_JOBS 5000
+
+
+//Declaración de funciones
+void escucharConeccionesJob();
+
+int main(){
+	escucharConeccionesJob();
+	return 0;
+}
+
+void escucharConeccionesJob(){
+	int sockfd, new_fd;  // Escuchar sobre sock_fd, nuevas conexiones sobre new_fd
+	struct sockaddr_in my_addr;    // información sobre mi dirección
+	struct sockaddr_in their_addr; // información sobre la dirección del cliente
+	unsigned int sin_size;
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { //si función socket devuelve -1 es error
+	            perror("socket");
+	            exit(1);
+	        }
+
+	my_addr.sin_family = AF_INET;         		// Ordenación de bytes de la máquina
+	my_addr.sin_port = htons(PUERTO_JOBS);     	// Puerto de escucha de Jobs. short, ordenación de bytes de la red
+	my_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 	// Mi dirección IP
+	memset(&(my_addr.sin_zero), '\0', 8); 				// Poner a cero el resto de la estructura
+
+	if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))== -1) { //Si la función bind devuelve -1 es error
+	            perror("bind");
+	            exit(1);
+	        }
+	if (listen(sockfd, 10) == -1) { //Si la función listen devuelve -1 es error
+	            perror("listen");
+	            exit(1);
+	        }
+
+	sin_size = sizeof(struct sockaddr_in);
+	new_fd = accept(sockfd, (struct sockaddr *)&their_addr,&sin_size);
+	if (new_fd == -1) {   //si accept devuelve -1 es error
+		perror("accept");
+	}
+	printf("server: got connection from IP:%d\n",inet_ntoa(their_addr.sin_addr));
+
+}
