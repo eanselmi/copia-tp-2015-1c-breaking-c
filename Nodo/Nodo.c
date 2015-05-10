ccb5df0b7bc1f004b5bb15eaeb857302605f6eb3
Bruno Di Stefano <bsdistefano@gmail.com>
2015-05-10 20:23:10 -0300

    agrego el archivo conf del Nodo

--- Nodo/Nodo.c
+++ Nodo/Nodo.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/config.h>
 
-//#include <breaking-C-library/>
+//Declaración de funciones
+void configurar();
+
+//Declaración de variables
+t_config* configurador;
+
+int main(){
+	configurar();
+	return 0;
+}
+
+void configurar(){
+	configurador= config_create("resources/nodoConfig.conf"); //se asigna el archivo de configuración especificado en la ruta
+		printf("ARCHIVO_BIN=%s\n",config_get_string_value(configurador,"ARCHIVO_BIN")); //para probar que realmente tomó al archivo de configuración
+	}
+
