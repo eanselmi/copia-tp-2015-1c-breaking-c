d0412c2b109172e0cf2ca7629a5d0b6033e41b11
Nazareno <nazareno.anselmi@gmail.com>
2015-05-12 02:10:20 -0300

    Primera parte del filesystem, espera a que se conecte la lista de nodos del archivo de configuracion, aun no funciona tengo problemas con las common de config

--- FileSystem/FileSystem.c
+++ FileSystem/FileSystem.c
@@ -8,31 +8,110 @@
 #include <netinet/in.h>
 #include <commons/collections/list.h>
 #include "FS_MDFS.h"
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
 
+#define BUF_SIZE 50
 #define BLOCK_SIZE 20971520
 
 
 //Declaración de Funciones
 int Menu();
 void DibujarMenu();
-void configurar();
 
-//Declaración de variables
+//Declaración de variables globales
 t_config * configurador;
+t_log* logger;
+fd_set master; // conjunto maestro de descriptores de fichero
+fd_set read_fds; // conjunto temporal de descriptores de fichero para select()
 
-int main(void){
-	configurar();
+int main(int argc , char *argv[]){
+	FD_ZERO(&master); // borra los conjuntos maestro y temporal
+	FD_ZERO(&read_fds);
+	struct sockaddr_in filesystem; // dirección del servidor
+	struct sockaddr_in nodo; // dirección del cliente
+	int fdmax; // número máximo de descriptores de fichero
+	int listener; // descriptor de socket a la escucha
+	int newfd; // descriptor de socket de nueva conexión aceptada
+	unsigned char identificacion[BUF_SIZE]; // buffer para datos del cliente
+	int yes=1; // para setsockopt() SO_REUSEADDR, más abajo
+	int i, j;
+	int nodos_iniciales=0;
+	int control_nodos=0;
+	char ** lista_ips;
+	configurador= config_create("resources/fsConfig.conf"); //se asigna el archivo de configuración especificado en la ruta
+	lista_ips = config_get_array_value (configurador,"LISTA_NODOS");
+
+	//....................................................................................
+
+	if ((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket");
+		log_info(logger,"FALLO la creacion del socket");
+		exit(-1);
+	}
+	// obviar el mensaje "address already in use" (la dirección ya se está usando)
+	if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes,sizeof(int)) == -1) {
+		perror("setsockopt");
+		log_info(logger,"FALLO la ejecucion del setsockopt");
+		exit(-1);
+	}
+	// enlazar
+	filesystem.sin_family = AF_INET;
+	filesystem.sin_addr.s_addr = INADDR_ANY;
+	filesystem.sin_port = htons(config_get_int_value(configurador,"PUERTO_LISTEN"));
+	memset(&(filesystem.sin_zero), '\0', 8);
+	if (bind(listener, (struct sockaddr *)&filesystem, sizeof(filesystem)) == -1) {
+		perror("bind");
+		log_info(logger,"FALLO el Bind");
+		exit(-1);
+	}
+	// escuchar
+	if (listen(listener, 10) == -1) {
+		perror("listen");
+		log_info(logger,"FALLO el Listen");
+		exit(1);
+	}
+	// añadir listener al conjunto maestro
+	FD_SET(listener, &master);
+	// seguir la pista del descriptor de fichero mayor
+	fdmax = listener; // por ahora es éste el ultimo socket
+
+	while (nodos_iniciales != strlen(lista_ips)){
+		if ((newfd = accept(listener, (struct sockaddr*)&nodo,&nodo)) == -1) {
+			perror ("accept");
+			log_info(logger,"FALLO el ACCEPT");
+		   	exit (-1);
+		}
+		for (i=1;i<=strlen(lista_ips);i++){
+			if (strcmp(inet_ntoa(nodo.sin_addr),lista_ips[i]))
+			//if (inet_ntoa(nodo.sin_addr)==config_get_array_value[i])
+				nodos_iniciales++;
+		}
+		if (nodos_iniciales == control_nodos)
+			free(newfd);
+		else {
+			control_nodos++;
+			FD_SET(newfd, &master);
+			fdmax = newfd;
+			free (newfd);
+		}
+	}
+
+	//Cuando sale de este ciclo el proceso FileSystem ya se encuentra en condiciones de iniciar sus tareas
+
+
+
+
+
+	//................................................................................
+
 	Menu();
+	log_destroy(logger);
 	return 0;
 }
-
-void configurar(){
-	configurador= config_create("resources/fsConfig.conf"); //se asigna el archivo de configuración especificado en la ruta
-	printf("COMBINER=%s\n",config_get_string_value(configurador,"COMBINER")); //para probar que realmente tomó al archivo de configuración
-}
-
-
-
 
 //Consola Menu
 void DibujarMenu(void){
@@ -76,7 +155,3 @@ int Menu(void){
 	}
 	return 0;
 }
-
-
-
-
