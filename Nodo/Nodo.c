6e78697c8045f17eadaab7a45bd655f8f1183b02
Nazareno <nazareno.anselmi@gmail.com>
2015-05-11 21:17:17 -0300

    Prepare la conexion del nodo con el FS, tambien saque la funcion que tomaba las configuraciones porque solo tiene 1 linea, la puse en el main al principio

--- Nodo/Nodo.c
+++ Nodo/Nodo.c
@@ -3,33 +3,84 @@
 #include <commons/config.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <commons/log.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
 #define BLOCK_SIZE 20971520
-
+#define BUF_SIZE 50
 
 //Declaración de funciones
-void configurar();
 char* mapearFileDeDatos();
-//void pruebammap();
-void leerDeMmap();
+//void pruebammap(); la comente porque como no esta desarrollada me da mensajes que no me gustan
 
-//Declaración de variables
+//void leerDeMmap(); la comente porque como no esta desarrollada me da mensajes que no me gustan
+
+//Declaración de variables Globales
 t_config* configurador;
+t_log* logger;
 int sizeFileDatos;
 
-int main(){
+int main(int argc , char *argv[]){
 	char* fileDeDatos;
-	configurar();
+	configurador= config_create("resources/nodoConfig.conf"); //se asigna el archivo de configuración especificado en la ruta
+
+	//-------------------------- Cuerpo ppal del programa ---------------------------------------
+
+	//------------ Variables locales a la funcion main --------------------
+	int sockfd;
+	unsigned char identificacion[BUF_SIZE]; //para el mensaje que envie al conectarse para identificarse, puede cambiar
+	struct sockaddr_in filesystem;
+	logger = log_create("./nodoLog.log", "Nodo", true, LOG_LEVEL_INFO);
+	memset(&filesystem, 0, sizeof(filesystem));
+	//---------------------------------------------------------------------
+
+	//Estructura para conexion con FS
+	filesystem.sin_family = AF_INET;
+	filesystem.sin_addr.s_addr = inet_addr(config_get_string_value(configurador,"IP_FS"));
+	filesystem.sin_port = htons(config_get_int_value(configurador,"PUERTO_FS"));
+	//-------------------------------
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror ("socket");
+		log_info(logger,"FALLO la creacion del socket");
+		exit (-1);
+	}
+	if (connect(sockfd, (struct sockaddr *)&filesystem,sizeof(struct sockaddr)) == -1) {
+		perror ("connect");
+		log_info(logger,"FALLO la conexion con el FS");
+		exit (-1);
+	}
+	log_info(logger,"Se conectó al FS IP: %s, en el puerto: %d",config_get_string_value(configurador,"IP_FS"),config_get_int_value(configurador,"PUERTO_FS"));
+	// aca revisaria si el nodo es nuevo o si es un nodo que se esta reconectando y dependiendo el caso, envia un mensaje y otro
+
+	if (1){ //algo que verifique si el nodo es nuevo o si esta reconectando, pongo 1 para que haga algo
+			//si el if da verdadero por nodo nuevo
+			strcpy(identificacion,"nuevo");
+			if((send(sockfd,identificacion,sizeof(identificacion),0))==-1) {
+					perror("send");
+					log_info(logger,"FALLO el envio del saludo al FS");
+					exit(-1);
+			}
+		}
+		else {
+			//si el if da da falso por nodo existente que se esta reconectando
+			strcpy(identificacion,"reconectado");
+			if((send(sockfd,identificacion,sizeof(identificacion),0))==-1) {
+					perror("send");
+					log_info(logger,"FALLO el envio del saludo al FS");
+					exit(-1);
+			}
+		}
+
 	fileDeDatos= mapearFileDeDatos(); //va a mapear el archivo de datos a memoria, y asignarle al puntero fileDeDatos la direccion donde arranca el file
 	escribirEnArchivo(fileDeDatos); //Escribe en la direccion de memoria que apunta fileDeDatos
 	leerDeArchivo(fileDeDatos); //Lee desde la direccion de memoria que apunta fileDeDatos
+	log_destroy(logger);
 	return 0;
 }
-
-void configurar(){
-	configurador= config_create("resources/nodoConfig.conf"); //se asigna el archivo de configuración especificado en la ruta
-		printf("ARCHIVO_BIN=%s\n",config_get_string_value(configurador,"ARCHIVO_BIN")); //para probar que realmente tomó al archivo de configuración
-	}
 
 char* mapearFileDeDatos(){
 	char* fileDatos;
@@ -55,7 +106,6 @@ void escribirEnArchivo(char* fileDeDatos){
 	memcpy(fileDeDatos,datos,BLOCK_SIZE); //Copia el valor de BLOCK_SIZE bytes desde la direccion de memoria apuntada por datos a la direccion de memoria apuntada por fileDeDatos
 	return;
 }
-
 
 void leerDeArchivo(char* fileDeDatos){
 	char* datosLeidos;
