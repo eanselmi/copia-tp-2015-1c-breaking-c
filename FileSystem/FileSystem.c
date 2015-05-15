157620f7fdb685a51b5c626bdf3aa89236bdca0a
ramosandrea <ramos.andreagisele@gmail.com>
2015-05-14 23:54:25 -0300

    Separo las opciones de la consola  para luego poder empezar a subir las funciones para cada una

--- FileSystem/FileSystem.c
+++ FileSystem/FileSystem.c
@@ -158,42 +158,58 @@ int main(int argc , char *argv[]){
 
 //Consola Menu
 void DibujarMenu(void){
-	printf("################################################################\n");
-	printf("# Ingrese una opción para continuar:                           #\n");
-	printf("# 1) Formatear el MDFS                                         #\n");
-	printf("# 2) Eliminar, Renombrar o Mover archivos                      #\n");
-	printf("# 3) Crear, Eliminar, Renombrar o Mover directorios            #\n");
-	printf("# 4) Copiar un archivo local al MDFS                           #\n");
-	printf("# 5) Copiar un archivo del MDFS al filesystem local            #\n");
-	printf("# 6) Solicitar el MD5 de un archivo en MDFS                    #\n");
-	printf("# 7) Ver, Borrar, Copiar los bloques que componen un archivo   #\n");
-	printf("# 8) Agregar un nodo de datos                                  #\n");
-	printf("# 9) Eliminar un nodo de datos                                 #\n");
-	printf("# 10) Salir                                                    #\n");
-	printf("################################################################\n");
+	void DibujarMenu(void){
+	  printf("################################################################\n");
+	  printf("# Ingrese una opción para continuar:                           #\n");
+	  printf("#  1) Formatear el MDFS                                        #\n");
+	  printf("#  2) Eliminar archivos                                        #\n");
+	  printf("#  3) Renombrar archivos                                       #\n");
+	  printf("#  4) Mover archivos                                           #\n");
+	  printf("#  5) Crear directorios                                        #\n");
+	  printf("#  6) Eliminar directorios                                     #\n");
+	  printf("#  7) Renombrar directorios                                    #\n");
+	  printf("#  8) Mover directorios                                        #\n");
+	  printf("#  9) Copiar un archivo local al MDFS                          #\n");
+	  printf("# 10) Copiar un archivo del MDFS al filesystem local           #\n");
+	  printf("# 11) Solicitar el MD5 de un archivo en MDFS                   #\n");
+	  printf("# 12) Ver los bloques que componen un archivo                  #\n");
+	  printf("# 13) Borrar los bloques que componen un archivo               #\n");
+	  printf("# 14) Copiar los bloques que componen un archivo               #\n");
+	  printf("# 15) Agregar un nodo de datos                                 #\n");
+	  printf("# 16) Eliminar un nodo de datos                                #\n");
+	  printf("# 17) Salir                                                    #\n");
+	  printf("################################################################\n");
+	}
 }
 
 int Menu(void){
 	char opchar[20];
 	int opcion=0;
-	while (opcion !=10){
-		sleep(1);
-		DibujarMenu();
-		printf("Ingrese opción: ");
-		scanf ("%s", opchar);
-		opcion = atoi (opchar);
-		switch (opcion){
-			case 1: printf("Eligió  Formatear el MDFS\n"); break;
-			case 2: printf("Eligió Eliminar, Renombrar o Mover archivos\n"); break;
-			case 3: printf("Eligió Crear, Eliminar, Renombrar o Mover directorios\n"); break;
-			case 4: printf("Eligió Copiar un archivo local al MDFS\n"); break;
-			case 5: printf("Eligió Copiar un archivo del MDFS al filesystem local\n"); break;
-			case 6: printf("Eligió Solicitar el MD5 de un archivo en MDFS\n"); break;
-			case 7: printf("Eligió Ver, Borrar, Copiar los bloques que componen un archivo\n"); break;
-			case 8: printf("Eligió Agregar un nodo de datos\n"); break;
-			case 9: printf("Eligió Eliminar un nodo de datos\n"); break;
-			case 10: printf("Eligió Salir\n"); break;
-			default: printf("Opción incorrecta. Por favor ingrese una opción del 1 al 10\n");break;
+	while (opcion !=17){
+	    sleep(1);
+	    DibujarMenu();
+	    printf("Ingrese opción: ");
+	    scanf ("%s", opchar);
+	    opcion = atoi (opchar);
+	    switch (opcion){
+	      case 1: printf("Eligió  Formatear el MDFS\n"); break;
+	      case 2: printf("Eligió Eliminar archivos \n"); break;
+	      case 3: printf("Eligió Renombrar archivos\n"); break;
+	      case 4: printf("Eligió Mover archivos\n"); break;
+	      case 5: printf("Eligió Crear directorios\n"); break;
+	      case 6: printf("Eligió Eliminar directorios\n"); break;
+	      case 7: printf("Eligió Renombrar directorios\n"); break;
+	      case 8: printf("Eligió Mover directorios\n"); break;
+	      case 9: printf("Eligió Copiar un archivo local al MDFS\n"); break;
+	      case 10: printf("Eligió Copiar un archivo del MDFS al filesystem local\n"); break;
+	      case 11: printf("Eligió Solicitar el MD5 de un archivo en MDFS\n"); break;
+	      case 12: printf("Eligió Ver los bloques que componen un archivo\n"); break;
+	      case 13: printf("Eligió Borrar los bloques que componen un archivo\n"); break;
+	      case 14: printf("Eligió Copiar los bloques que componen un archivo\n"); break;
+	      case 15: printf("Eligió Agregar un nodo de datos\n"); break;
+	      case 16: printf("Eligió Eliminar un nodo de datos\n"); break;
+	      case 17: printf("Eligió Salir\n"); break;
+	      default: printf("Opción incorrecta. Por favor ingrese una opción del 1 al 17\n");break;
 		}
 	}
 	return 0;
