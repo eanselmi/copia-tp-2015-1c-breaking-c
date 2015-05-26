fd195285a9dc1138f86dcd5e0003e8080184f24c
Bruno Di Stefano <bsdistefano@gmail.com>
2015-05-26 01:06:45 -0300

    Acomodo un poco la conexión de Job con Marta

--- Job/Job.c
+++ Job/Job.c
@@ -6,54 +6,43 @@
 
 
 //Declaración de funciones
-void conectaraMarta();
-void desconectarDeMarta();
-void configurar();
 
 //Declaración de variables
 t_config* configurador;
+t_log* logger;
+
 
 int main(void){
-	configurar();
-	conectaraMarta(); //Se conecta a MaRTA y se guarda en el Log
-	desconectarDeMarta(); //solo log
-	return 0;
-}
-
-void configurar(){
 	configurador= config_create("resources/jobConfig.conf"); //se asigna el archivo de configuración especificado en la ruta
-	printf("COMBINER=%s\n",config_get_string_value(configurador,"COMBINER")); //para probar que realmente tomó al archivo de configuración
-}
+	logger = log_create("./jobLog.log", "Job", true, LOG_LEVEL_INFO); //se crea la instancia de log, que tambien imprimira en pantalla
+	//Variables locales a main
+	int marta_sock; //socket de conexión a MaRTA
+	struct sockaddr_in marta_addr;
 
-void conectaraMarta(){
-	int sockfd, numbytes, new_fd;
-	struct sockaddr_in their_addr;
+	/* Se conecta a MaRTA */
 
-	if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1){ //si función socket devuelve -1 es error
-        perror("socket");
-        exit(1);
+	if((marta_sock=socket(AF_INET,SOCK_STREAM,0))==-1){ //si función socket devuelve -1 es error
+	       perror("socket");
+	       log_error(logger,"Fallo la creación del socket");
+	       exit(1);
 	}
-	their_addr.sin_family=AF_INET;
-	their_addr.sin_port=htons(config_get_int_value(configurador,"PUERTO_MARTA"));
-	their_addr.sin_addr.s_addr=inet_addr(config_get_string_value(configurador,"IP_MARTA"));
-	memset(&(their_addr.sin_zero),'\0',8);
 
-	if((new_fd=connect(sockfd,(struct sockaddr *)&their_addr,sizeof(struct sockaddr)))==-1){
+	marta_addr.sin_family=AF_INET;
+	marta_addr.sin_port=htons(config_get_int_value(configurador,"PUERTO_MARTA"));
+	marta_addr.sin_addr.s_addr=inet_addr(config_get_string_value(configurador,"IP_MARTA"));
+	memset(&(marta_addr.sin_zero),'\0',8);
+
+	if((connect(marta_sock,(struct sockaddr *)&marta_addr,sizeof(struct sockaddr)))==-1){
 		perror("connect");
+		log_error(logger,"Fallo la conexión con MaRTA");
 		exit(1);
 	}
+	/*Conexión con MaRTA establecida*/
 
-	t_log* logger;
-	logger = log_create("./jobLog.log", "Job", true, LOG_LEVEL_INFO); //se crea la instancia de log, que tambien imprimira en pantalla
-	log_info(logger,"Se conectó a MaRTA. IP: %s, Puerto: %d",config_get_string_value(configurador,"IP_MARTA"),config_get_int_value(configurador,"PUERTO_MARTA")); //se agrega al log en modo de informacion la coneccion con MaRTA
-	log_destroy(logger); //se elimina la instancia de log
-	return;
-}
+	log_info(logger,"Se conectó a MaRTA. IP: %s, Puerto: %d",config_get_string_value(configurador,"IP_MARTA"),config_get_int_value(configurador,"PUERTO_MARTA")); //se agrega al log en modo de informacion la conexión con MaRTA
 
-void desconectarDeMarta(){
-	t_log* logger;
-	logger = log_create("./jobLog.log", "Job", true, LOG_LEVEL_INFO); //se crea la instancia de log, que tambien imprimira en pantalla
-	log_info(logger,"Se desconectó de MaRTA. IP: %s, Puerto: %d",config_get_string_value(configurador,"IP_MARTA"),config_get_int_value(configurador,"PUERTO_MARTA")); //se agrega al log en modo de informacion la coneccion con MaRTA
+
+
 	log_destroy(logger); //se elimina la instancia de log
-	return;
+	return 0;
 }
