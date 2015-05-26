be335ea6043c2439fede75efd548d9902cdc4171
Bruno Di Stefano <bsdistefano@gmail.com>
2015-05-26 17:37:15 -0300

    Las agrego

---
+++ Job/Job.h
@@ -0,0 +1,19 @@
+typedef struct estructura_mapper {
+	char* ip_nodo;
+	int puerto_nodo;
+	char* nombreArchivoTemporal;
+} t_mapper;
+
+typedef struct estructura_reduce {
+	char* ip_nodoPpal;
+	int puerto_nodoPpal;
+	char* archivoAAplicarReduceLocal;
+	t_list* listaNodos; //una lista que tenga los otros nodos y archivos a donde aplicar reduce (lista de t_reduce_otrosnodos)
+	char* nombreArchivoFinal;
+} t_reduce;
+
+typedef struct lista_nodos_reduce{
+	char* ip_nodo;
+	int puerto_nodo;
+	char* archivoAAplicarReduce;
+} t_reduce_otrosnodos;
