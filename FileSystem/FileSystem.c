dc1d272cb831686abce63c85fb4d93f0ff27da6d
Nazareno <nazareno.anselmi@gmail.com>
2015-06-02 23:40:42 -0300

    Agrego la funcion obtener_md5

--- FileSystem/FileSystem.c
+++ FileSystem/FileSystem.c
@@ -31,6 +31,7 @@ void *connection_handler_escucha(); // Esta funcion escucha continuamente si rec
 static t_nodo *agregar_nodo_a_lista(int socket,int est,char *ip, int port, int bloques_lib, int bloques_tot);
 void modificar_estado_nodo (int socket,char *ip,int port,int estado);
 void listar_nodos_conectados(t_list *nodos);
+char *obtener_md5(char *archivo);
 void formatear_nodos(void);
 void FormatearFilesystem ();		//Pame TODAVIA NO DESARROLLADA
 void EliminarArchivo();				//DESARROLLADA
@@ -301,6 +302,29 @@ static t_nodo *agregar_nodo_a_lista(int socket,int est,char *ip, int port, int b
 	}
 
 	return nodo_temporal;
+}
+
+char *obtener_md5(char *archivo){
+	int fd[2];
+	int childpid;
+	char *resultado;
+	pipe(fd);
+	char result[1000];
+	memset(result,'\0',1000);
+	if ( (childpid = fork() ) == -1){
+		fprintf(stderr, "Fallo el FORK");
+	} else if( childpid == 0) {
+		close(1);
+		dup2(fd[1], 1);
+		close(fd[0]);
+		execlp("/usr/bin/md5sum","md5sum",archivo,NULL);
+	}
+	wait(NULL);
+	read(fd[0], result, sizeof(result));
+	printf("%s",result);
+	resultado=malloc(sizeof(result));
+	strcpy(resultado,result);
+	return resultado;
 }
 
 void listar_nodos_conectados(t_list *nodos){
