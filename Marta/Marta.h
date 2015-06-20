7337a470dc3b9a10c9b3be9a8b9614e2509aa726
ramosandrea <ramos.andreagisele@gmail.com>
2015-06-19 21:58:52 -0300

    Agrego estructuras de Marta para recibir archivos y nodos de Filesystem

--- Marta/Marta.h
+++ Marta/Marta.h
@@ -2,6 +2,35 @@
 #define MENSAJE_SIZE 4096
 #define TAM_NOMFINAL 60
 
+//estructura con la información de FileSystem
+typedef struct estructura_filesystem {
+	char* nombre;
+	char* path;
+	uint32_t estado;
+	t_list *bloques; //Se debe crear una lista de tipo "t_bloque" y agregarla acá
+} t_archivo;
+
+typedef struct estructura_copia {
+	char* nodo;
+	int bloqueNodo;
+} t_copias;
+
+typedef struct estructura_bloque {
+	t_list *copias;
+} t_bloque;
+
+typedef struct estructura_manejo_nodos {
+	char nodo_id[6];
+	int estado;
+	char *ip;
+	int puerto_escucha_nodo;
+	uint32_t cantMappers;	//cantidad de map corriendo en el nodo
+	uint32_t cantReducers;  //cantidad de reduce corriendo en el nodo
+} t_nodo;
+
+
+
+
 //Estructura para manejar los Jobs
 
 typedef struct estructura_job{
