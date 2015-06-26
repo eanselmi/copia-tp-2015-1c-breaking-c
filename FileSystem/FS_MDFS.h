0e79a9256a7bd37a7510c3f58d51f9afa5f296f8
Nazareno <nazareno.anselmi@gmail.com>
2015-06-25 23:59:42 -0300

    comentarios para andy

--- FileSystem/FS_MDFS.h
+++ FileSystem/FS_MDFS.h
@@ -62,8 +62,8 @@ int CopiarArchivoAMDFS();								//DESARROLLADA, falta persistencia      1. mand
 int CopiarArchivoDelMDFS(int flag, char*unArchivo);		//DESARROLLADA
 void MD5DeArchivo();									//DESARROLLADA
 int VerBloque();										//DESARROLLADA
-void BorrarBloque();									//DESARROLLADA, falta persistencia      /ver que responden de bloque
-void CopiarBloque();									//DESARROLLADA, falta persistencia      /ver que responden de bloque
+void BorrarBloque();									//DESARROLLADA, falta persistencia
+void CopiarBloque();									//DESARROLLADA, falta persistencia      1. mandar nuevo_bloque 2. archivo 3. padre 4. n_bloque 5.nodo_id 6. bloque_nuevo
 void AgregarNodo();										//DESARROLLADA
 void EliminarNodo();  									//DESARROLLADA
 void eliminar_listas(t_list *archivos_l, t_list *directorios_d, t_list *nodos_n);	//DESARROLLADA
