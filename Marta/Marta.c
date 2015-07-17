a0a93267e5ce5afe31d85aab2307003157846379
Bruno Di Stefano <bsdistefano@gmail.com>
2015-07-17 16:44:11 -0300

    Validacion si no existe archivo en MDFS

--- Marta/Marta.c
+++ Marta/Marta.c
@@ -1045,18 +1045,22 @@ void *atenderJob (int *socketJob) {
 		}
 
 //		//Validacion si existe el archivo
-//		if(padre==-1){
-//                        char msjJob[BUF_SIZE];
-//                        memset(msjJob,'\0',BUF_SIZE);
-//                        strcpy(msjJob,"arch no disp");
-//                        log_warning(logger,"El archivo %s no existe en el MDFS",archivoAPedirPadre);
-//                        if(send(*socketJob,msjJob,sizeof(msjJob),MSG_WAITALL)==-1){
-//                        	perror("Send");
-//                                log_error(logger,"Fallo el envio del mensaje \"archivo no disponible\" al job");
-//                        }
-//                        close(*socketJob);
-//			pthread_exit((void*)0);
-//		}
+		if(padre==-1){
+			char msjJob[BUF_SIZE];
+			memset(msjJob,'\0',BUF_SIZE);
+			strcpy(msjJob,"arch no disp");
+			log_warning(logger,"El archivo %s no existe en el MDFS",archivoAPedirPadre);
+			if(send(*socketJob,msjJob,sizeof(msjJob),MSG_WAITALL)==-1){
+				perror("Send");
+				log_error(logger,"Fallo el envio del mensaje \"archivo no disponible\" al job");
+			}
+			close(*socketJob);
+			nuevoJob->mapperPendientes=0;
+			nuevoJob->reducePendientes=0;
+			memset(nuevoJob->estado,'\0',10);
+			strcpy(nuevoJob->estado,"Fallido");
+			pthread_exit((void*)0);
+		}
 
 		//De cada archivo que nos manda el Job buscamos y nos traemos los bloques
 
